4e4f0f70075ca093f37c4a9437ba57bc36ef7f3e
liwenka1 <2020583117@qq.com>
2025-12-01 23:09:22 +0800

    chore: 更新 .editorconfig 文件以支持中文注释和 md 文件特定规则; 修改 eslint 配置以关闭显式函数返回类型规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,7 @@ export default defineConfig(
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
       ...eslintPluginReactRefresh.configs.vite.rules,
+      "@typescript-eslint/explicit-function-return-type": "off",
       "react-refresh/only-export-components": "off"
     }
   },
